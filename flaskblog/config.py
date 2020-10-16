797835f3f6b20af25bd3963914ba43eee1302650
Yola21 <yashkhorja4@gmail.com>
2020-10-16 19:30:04 +0530

    postgre db

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,8 @@ import os
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
